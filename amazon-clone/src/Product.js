08eca6b078833194d8f828ec5afa5e555a03b1cd
apoorvpathak <apoorvpathak.apk@gmail.com>
2021-03-10 19:34:45 +0530

    made changes to design

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
 function Product() {
-    return(
-        <div className="product">
-            <div className="product__info">
-                <p>
-                    Hooked: How to Build Habit-Forming Products
-                </p>
-                <p className="product__price">
-                    <small>$</small>
-                    <strong>13.99</strong>
-                </p>
-                <div className="product__rating">
-                    <p>⭐</p>
-                </div>
-                <img 
-                src="https://m.media-amazon.com/images/I/81FU4zAWJvL._AC_UY218_.jpg" alt=""/>
-
-                <button>Add to basket</button>
-            </div>
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>Hooked</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>13.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>⭐</p>
         </div>
-    )
+      </div>
+
+      <img
+        src="https://m.media-amazon.com/images/I/81FU4zAWJvL._AC_UY218_.jpg"
+        alt=""
+      />
+
+      <button>Add to basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
