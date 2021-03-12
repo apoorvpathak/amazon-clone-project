4bf826f59a3a65cc34d09e42258ad85e9e0ed45c
apoorvpathak <apoorvpathak.apk@gmail.com>
2021-03-12 18:38:43 +0530

    Update Checkout.js

---
+++ amazon-clone/src/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Checkout.css";
+
+
+function Checkout() {
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img className src="" alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
