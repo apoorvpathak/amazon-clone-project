ef1bb1a7ca2def599e7c79df441cdb5650b4b8c2
apoorvpathak <apoorvpathak.apk@gmail.com>
2021-03-12 18:38:27 +0530

    Update App.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           
           <Route path="/checkout">
             <Header />
-            <Checkout />
+            <Checkout/>
           </Route>
           
           
